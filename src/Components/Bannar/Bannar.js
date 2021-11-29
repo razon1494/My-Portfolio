1e1eabeb465439dd382fc4d670f6e4ea8713460e
razon1494 <razon1494@gmail.com>
2021-11-29 18:29:54 +0600

    Banner responsive done

--- src/Components/Bannar/Bannar.js
+++ src/Components/Bannar/Bannar.js
@@ -5,24 +5,24 @@ const Bannar=() => {
     return (
         <div>
             <div className="main-bannar row ">
-            <div className="d-flex my-5 justify-content-center col-md-7">
+            <div className="d-md-flex my-5 order-md-first justify-content-center col-md-7 col-12 ">
         <div className="writing-part">
         <p className='small-title'>Design & Develop Your Dream Website</p>
         <h1 className='main-title'>Hi, This is <span className='arifur' >Arifur Rahman</span></h1>
         <div className="wrapper">
-            <div className="static-text">I'm a</div>
-            <ul className="dynamic-texts">
-                  <li> <span>MERN Stack Developer</span> </li>
-                  <li> <span>Web Developer</span> </li>
-                  <li> <span>Front-End Developer</span> </li>
-                  <li> <span>React Developer</span> </li>
+            {/* <div className="static-text fs-1">I'm a</div> */}
+            <ul className="dynamic-texts fs-1">
+                  <li><span className='fs-1'>Web Developer</span></li>
+                  <li><span className='fs-1'>Front-End Developer</span></li>
+                  <li><span className='fs-1'>MERN Stack Developer</span></li>
+                  <li><span className='fs-1'>React Developer</span></li>
             </ul>
                         </div>
                         <br />
 
         <button class="my-3 button1 button-801">Hire Me</button>
        <a href="https://drive.google.com/file/d/1pUr717Hxw3qbDjNpFdbUJZkhVnaZk3yj/view?usp=sharing" target="blank" download><button class="button-80 mx-3 my-3"> My Resume</button></a></div></div>
-                <div className="img-part col-md-5 ">
+                <div className="img-part col-md-5 col-12 order-first ">
                     <img className='img-fluid rounded-circle my-img' src={bannar} alt="" />
                 </div>
             </div>
---
+++ src/Components/MyStrength/MyStrength.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyStrength = () => {
+    return (
+        <div>
+            <h2>Look All of my expertise topics</h2>
+        </div>
+    );
+};
+
+export default MyStrength;
